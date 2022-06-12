5985c078bcb7c7bf6a96948ea5c7edb568a5bc4a
Gabriel Carmelo <Gabriel.Carmelo@pe.ey.com>
2022-06-12 18:28:33 -0500

    End of section 8

--- client-app/src/features/activities/dashboard/ActivityList.tsx
+++ client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -1,16 +1,17 @@
 import { observer } from 'mobx-react-lite';
 import React, { SyntheticEvent, useState } from 'react';
+import { Link } from 'react-router-dom';
 import { Button, Item, Label, Segment } from 'semantic-ui-react';
 import { useStore } from '../../../app/stores/store';
 
 
 export default observer(function ActivityList() {
     const { activityStore } = useStore();
-    const {deleteActivity, activitiesByDate, loading} = activityStore;
+    const { deleteActivity, activitiesByDate, loading } = activityStore;
 
     const [target, setTarget] = useState('');
 
-    function handleActivityDelete(e: SyntheticEvent<HTMLButtonElement>, id: string){
+    function handleActivityDelete(e: SyntheticEvent<HTMLButtonElement>, id: string) {
         setTarget(e.currentTarget.name);
         deleteActivity(id);
     }
@@ -27,7 +28,13 @@ export default observer(function ActivityList() {
                                 <div>{activity.city}, {activity.venue}</div>
                             </Item.Description>
                             <Item.Extra>
-                                <Button onClick={() => activityStore.selectActivity(activity.id)} floated='right' content="View" color="blue"></Button>
+                                <Button
+                                    as={Link}
+                                    to={`/activities/${activity.id}`}
+                                    floated='right'
+                                    content="View"
+                                    color="blue"
+                                />
                                 <Button
                                     name={activity.id}
                                     onClick={(e) => handleActivityDelete(e, activity.id)}
--- client-app/src/features/activities/details/ActivityDetails.tsx
+++ client-app/src/features/activities/details/ActivityDetails.tsx
@@ -1,14 +1,21 @@
-import React from 'react';
+import { observer } from 'mobx-react-lite';
+import React, { useEffect } from 'react';
+import { Link, useParams } from 'react-router-dom';
 import { Button, ButtonGroup, Card, Image } from 'semantic-ui-react';
 import LoadingComponent from '../../../app/layout/LoadingComponent';
 import { useStore } from '../../../app/stores/store';
 
 
-export default function ActivityDetails(){
+export default observer(function ActivityDetails(){
     const { activityStore } = useStore();
-    const {selectedActivity: activity, openForm, cancelSelectedActivity} = activityStore;
+    const {selectedActivity: activity, loadActivity, loadingInitial} = activityStore;
+    const { id } = useParams<{id: string}>();
 
-    if (!activity) return <LoadingComponent content='Loading...' />;
+    useEffect(() => {
+        if(id) loadActivity(id);
+    }, [id, loadActivity]);
+
+    if (loadingInitial || !activity) return <LoadingComponent content='Loading...' />;
 
     return (
         <Card fluid>
@@ -24,10 +31,10 @@ export default function ActivityDetails(){
             </Card.Content>
             <Card.Content extra>
                 <ButtonGroup widths="2">
-                    <Button onClick={() => openForm(activity.id)} basic color="blue" content="Edit"/>
-                    <Button onClick={cancelSelectedActivity} basic color="grey" content="Cancel"/>
+                    <Button as={Link} to={`/manage/${activity.id}`} basic color="blue" content="Edit"/>
+                    <Button as={Link} to="activities" basic color="grey" content="Cancel"/>
                 </ButtonGroup>
             </Card.Content>
         </Card>
     )
-}
+})
