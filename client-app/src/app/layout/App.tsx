34915fa6982df70dfb5c81efff3844c01c02bdf1
Gabriel Carmelo <Gabriel.Carmelo@pe.ey.com>
2022-06-22 23:35:02 -0500

    End of section 10

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -3,10 +3,14 @@ import { Container } from 'semantic-ui-react';
 import NavBar from './NavBar';
 import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
 import { observer } from 'mobx-react-lite';
-import { Route, useLocation } from 'react-router-dom';
+import { Route, Switch, useLocation } from 'react-router-dom';
 import HomePage from '../../features/home/HomePage';
 import ActivityForm from '../../features/activities/form/ActivityForm';
 import ActivityDetails from '../../features/activities/details/ActivityDetails';
+import TestErrors from '../../features/error/TestError';
+import { ToastContainer } from 'react-toastify';
+import NotFound from '../../features/error/NotFound';
+import ServerError from '../../features/error/ServerError';
 
 function App() {
   const location = useLocation();
@@ -17,11 +21,17 @@ function App() {
         path={'/(.+)'}
         render={() => (
           <>
+            <ToastContainer position='bottom-right' hideProgressBar />
             <NavBar />
             <Container style={{ marginTop: "7em" }}>
-              <Route path="/activities" exact component={ActivityDashboard} />
-              <Route path="/activities/:id" component={ActivityDetails} />
-              <Route key={location.key} path={["/createActivity", "/manage/:id"]} component={ActivityForm} />
+              <Switch>
+                <Route path="/activities" exact component={ActivityDashboard} />
+                <Route path="/activities/:id" component={ActivityDetails} />
+                <Route key={location.key} path={["/createActivity", "/manage/:id"]} component={ActivityForm} />
+                <Route path={["/errors"]} component={TestErrors} />
+                <Route path={["/server-error"]} component={ServerError} />
+                <Route component={NotFound} />
+              </Switch>
             </Container>
           </>
         )}
