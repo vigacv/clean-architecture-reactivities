7da04d4f32e70121ba6a5160f9b51f3ad465265c
Gabriel Carmelo <Gabriel.Carmelo@pe.ey.com>
2022-06-29 14:20:07 -0500

    End of section 13

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect } from 'react';
 import { Container } from 'semantic-ui-react';
 import NavBar from './NavBar';
 import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
@@ -11,17 +11,34 @@ import TestErrors from '../../features/error/TestError';
 import { ToastContainer } from 'react-toastify';
 import NotFound from '../../features/error/NotFound';
 import ServerError from '../../features/error/ServerError';
+import LoginForm from '../../features/users/LoginForm';
+import { useStore } from '../stores/store';
+import LoadingComponent from './LoadingComponent';
+import ModalContainer from '../common/modals/ModalContainer';
 
 function App() {
   const location = useLocation();
+  const { commonStore, userStore } = useStore();
+
+  useEffect(() => {
+    if (commonStore.token) {
+      userStore.getUser().finally(() => commonStore.setAppLoaded())
+    } else {
+      commonStore.setAppLoaded();
+    }
+  }, [commonStore, userStore])
+
+  if (!commonStore.appLoaded) return <LoadingComponent content='Loading app...' />
+
   return (
     <>
+      <ToastContainer position='bottom-right' hideProgressBar />
+      <ModalContainer />
       <Route exact path="/" component={HomePage} />
       <Route
         path={'/(.+)'}
         render={() => (
           <>
-            <ToastContainer position='bottom-right' hideProgressBar />
             <NavBar />
             <Container style={{ marginTop: "7em" }}>
               <Switch>
@@ -30,6 +47,7 @@ function App() {
                 <Route key={location.key} path={["/createActivity", "/manage/:id"]} component={ActivityForm} />
                 <Route path={["/errors"]} component={TestErrors} />
                 <Route path={["/server-error"]} component={ServerError} />
+                <Route path={["/login"]} component={LoginForm} />
                 <Route component={NotFound} />
               </Switch>
             </Container>
--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -14,7 +14,7 @@ export default observer(function ActivityDashboard() {
         if(activityRegistry.size <= 1) activityStore.loadActivities();
     }, [activityRegistry.size, activityStore])
 
-    if (activityStore.loadingInitial) return <LoadingComponent content='Loading app' />
+    if (activityStore.loadingInitial) return <LoadingComponent content='Loading activities...' />
 
     return (
         <Grid>
