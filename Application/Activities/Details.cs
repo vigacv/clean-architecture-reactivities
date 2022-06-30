2e8657535f84c70ec04200b6d341f687f3ea39db
Gabriel Carmelo <Gabriel.Carmelo@pe.ey.com>
2022-06-29 22:32:28 -0500

    End of section 14

--- Application/Activities/Details.cs
+++ Application/Activities/Details.cs
@@ -2,32 +2,41 @@ using System;
 using System.Threading;
 using System.Threading.Tasks;
 using Application.Core;
+using AutoMapper;
 using Domain;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
 using Persistence;
 
 namespace Application.Activities
 {
     public class Details
     {
-        public class Query : IRequest<Result<Activity>>
+        public class Query : IRequest<Result<ActivityDto>>
         {
             public Guid Id { get; set; }
         }
 
-        public class Handler : IRequestHandler<Query, Result<Activity>>
+        public class Handler : IRequestHandler<Query, Result<ActivityDto>>
         {
             private readonly DataContext _context;
-            public Handler(DataContext context)
+            private readonly IMapper _mapper;
+            public Handler(DataContext context, IMapper mapper)
             {
+                _mapper = mapper;
                 _context = context;
             }
 
-            public async Task<Result<Activity>> Handle(Query request, CancellationToken cancellationToken)
+            public async Task<Result<ActivityDto>> Handle(Query request, CancellationToken cancellationToken)
             {
-                var activity = await _context.Activities.FindAsync(request.Id);
+                var activity = await _context.Activities
+                    .Include(a => a.Attendees)
+                    .ThenInclude(aa => aa.AppUser)
+                    .SingleOrDefaultAsync(a => a.Id == request.Id);
 
-                return Result<Activity>.Success(activity);
+                var activityToReturn = _mapper.Map<ActivityDto>(activity);
+
+                return Result<ActivityDto>.Success(activityToReturn);
             }
         }
     }
